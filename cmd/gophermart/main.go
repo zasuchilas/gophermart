60980d210a964e379ba5bd627680eba19b0b7d6c
zasuchilas <zasuchilas@ya.ru>
2024-11-07 12:47:48 +0300

    Fix connection refused (hello gophermart v8 rollback)

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,34 +1,27 @@
 package main
 
-import (
-	"github.com/zasuchilas/gophermart/internal/gophermart/config"
-	"github.com/zasuchilas/gophermart/internal/gophermart/logger"
-	"github.com/zasuchilas/gophermart/internal/gophermart/storage/pgstorage"
-	"io"
-	"log"
-	"net/http"
-)
+import "github.com/zasuchilas/gophermart/internal/gophermart"
 
 func main() {
-	config.ParseFlags()
-	log.Println("RUN_ADDRESS", config.RunAddress)
-
-	logger.Init()
-	logger.ServiceInfo("GOPHERMART (... service)", "TEST VERSION")
-
-	store := pgstorage.New()
-	log.Println("STORE", store.InstanceName())
-
-	//chisrv.InitJWT()
-	//log.Println("INIT JWT OK!")
-
-	log.Println("HELLO GOPHERMART!")
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		log.Println("GET: HELLO GOPHERMART!")
-		io.WriteString(w, "HELLO GOPHERMART!\n")
-	}
-	http.HandleFunc("/", helloHandler)
-	log.Fatal(http.ListenAndServe(config.RunAddress, nil))
-	//a := gophermart.New()
-	//a.Run()
+	//config.ParseFlags()
+	//log.Println("RUN_ADDRESS", config.RunAddress)
+	//
+	//logger.Init()
+	//logger.ServiceInfo("GOPHERMART (... service)", "TEST VERSION")
+	//
+	//store := pgstorage.New()
+	//log.Println("STORE", store.InstanceName())
+	//
+	////chisrv.InitJWT()
+	////log.Println("INIT JWT OK!")
+	//
+	//log.Println("HELLO GOPHERMART!")
+	//helloHandler := func(w http.ResponseWriter, req *http.Request) {
+	//	log.Println("GET: HELLO GOPHERMART!")
+	//	io.WriteString(w, "HELLO GOPHERMART!\n")
+	//}
+	//http.HandleFunc("/", helloHandler)
+	//log.Fatal(http.ListenAndServe(config.RunAddress, nil))
+	a := gophermart.New()
+	a.Run()
 }
