fbec78126d5c1db656f6b0eb736b54bc60ee42fe
zasuchilas <zasuchilas@ya.ru>
2024-11-07 11:49:29 +0300

    Fix connection refused (hello gophermart v2)

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "log"
+import (
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	log.Println("HELLO GOPHERMART!")
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		log.Println("GET: HELLO GOPHERMART!")
+		io.WriteString(w, "HELLO GOPHERMART!\n")
+	}
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 	//a := gophermart.New()
 	//a.Run()
 }
